73e15676bd584f93ba1f8ce61fa993c06d071e9c
fernaldr <121467203+fernaldr@users.noreply.github.com>
2023-11-03 16:29:31 -0400

    removed if statement since it broke

--- HW3/main.py
+++ HW3/main.py
@@ -18,17 +18,12 @@ def index():
    
     cursor, connection = util.connect_to_db(username,password,host,port,database)
     
-    if recordA == -1 or recordB ==-1:
-        
-        print('Something is wrong with the SQL command')
-    else:
-
-        recordA = util.run_and_fetch_sql(cursor, "SELECT * from basket_a;")
-        columns = [desc[0] for desc in cursor.description]
-        logA = [dict(zip(columns, row)) for row in recordB]
-        recordB = util.run_and_fetch_sql(cursor, "SELECT * from basket_b;")
-        columns = [desc[0] for desc in cursor.description]
-        logB = [dict(zip(columns, row)) for row in recordA]
+    recordA = util.run_and_fetch_sql(cursor, "SELECT * from basket_a;")
+    columns = [desc[0] for desc in cursor.description]
+    logA = [dict(zip(columns, row)) for row in recordB]
+    recordB = util.run_and_fetch_sql(cursor, "SELECT * from basket_b;")
+    columns = [desc[0] for desc in cursor.description]
+    logB = [dict(zip(columns, row)) for row in recordA]
         
     util.disconnect_from_db(connection, cursor)
     return render_template('index.html', logA=logA, logB=logB)

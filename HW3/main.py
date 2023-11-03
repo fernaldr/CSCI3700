6c4af00afe4408e8b00bda647aa80419f640f9d9
fernaldr <121467203+fernaldr@users.noreply.github.com>
2023-11-03 14:42:32 -0400

    test

--- HW3/main.py
+++ HW3/main.py
@@ -10,13 +10,73 @@ host='127.0.0.1'
 port='54321'
 database='testdb1'
 
-@app.route('/')
+def config(filename='database.ini', section='postgresql'):
+    # create a parser
+    parser = ConfigParser()
+    # read config file
+    parser.read(filename)
+
+    # get section, default to postgresql
+    db = {}
+    if parser.has_section(section):
+        params = parser.items(section)
+        for param in params:
+            db[param[0]] = param[1]
+    else:
+        raise Exception('Section {0} not found in the {1} file'.format(section, filename))
+
+    return db
+
+def connect():
+    """ Connect to the PostgreSQL database server """
+    conn = None
+    try:
+        print('Connecting to the PostgreSQL database...')
+        conn = psycopg2.connect(
+    host=host,
+    database="testdb1",
+    user=username,
+    password=password)
+		
+        # create a cursor
+        cur = conn.cursor()
+        
+	# execute a statement
+        print('PostgreSQL database version:')
+        cur.execute('SELECT version()')
+
+        # display the PostgreSQL database server version
+        db_version = cur.fetchone()
+        print(db_version)
+       
+	# close the communication with the PostgreSQL
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+            print('Database connection closed.')
+
+
+if __name__ == '__main__':
+    connect()
+
+"""@app.route('/')
 def index():
  # this is your index page
     # connect to DB
-    """cursor, connection = util.connect_to_db(username,password,host,port,database)"""
-    conn_string = "127.0.0.1:5000"
+
+    cursor, connection = util.connect_to_db(username,password,host,port,database)
+
+    conn = psycopg2.connect(
+    host=host,
+    database="testdb1",
+    user=username,
+    password=password)
+
     db =  psycopg2.connect(conn_string)
+
     cursor = db.cursor
     # execute SQL commands
     record = util.run_and_fetch_sql(cursor, "SELECT * from customer;")
@@ -41,8 +101,13 @@ def update_basket_a():
     conn_string = "127.0.0.1:5000/api/update_basket_a"
     db =  psycopg2.connect(conn_string)
     curs = db.cursor()
-    """cursor, connection = util.connect_to_db(username,password,host,port,database)"""
-    ins_statement = f"""INSERT INTO basket_a ("PRIMARY KEY", "fruit_a") VALUES (5, 'Cherry');"""
+
+
+    cursor, connection = util.connect_to_db(username,password,host,port,database)
+
+
+    ins_statement = fINSERT INTO basket_a ("PRIMARY KEY", "fruit_a") VALUES (5, 'Cherry');
+    
     curs.execute(ins_statement)
 
 @app.route('/unique', methods=['POST'])
@@ -50,7 +115,12 @@ def unique():
     conn_string = "127.0.0.1:5000/api/unique"
     db =  psycopg2.connect(conn_string)
     curs = db.cursor()
-    """cursor, connection = util.connect_to_db(username,password,host,port,database)"""
+    
+    
+    cursor, connection = util.connect_to_db(username,password,host,port,database)
+    
+
+
     record = util.run_and_fetch_sql(curs, "SELECT * from customer;")
     if record == -1:
         print('Something is wrong with the SQL command')
@@ -72,4 +142,4 @@ if __name__ == '__main__':
     app.debug = True
     # your local machine ip
     ip = '127.0.0.1'
-    app.run(host=ip)
+    app.run(host=ip) """
